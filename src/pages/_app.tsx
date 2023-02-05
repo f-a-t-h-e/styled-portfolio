461ed77a8e4317860017ff2353de33e674f59994
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-05 08:00:11 +0200

    Fixing issue. You need to delete .next folder then start the server again to proceed.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import GlobalStyle from "@/components/globalstyles";
+// import GlobalStyle from "@/styles/globalStyles";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#111",
+    secondary: "#0070f3",
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
